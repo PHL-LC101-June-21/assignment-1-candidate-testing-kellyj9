0cd859f14527e3a4691a4c3347c49c1f722332c0
kellyj9 <85304372+kellyj9@users.noreply.github.com>
2021-06-13 03:54:11 +0000

    Updated candidate-testing.js to include functionality to ask the user their name, greet them by name, ask one quiz question, and provide feedback on their answer.

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,11 +3,12 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer= "";
+
 let questions;
 let correctAnswers;
 let candidateAnswers;
@@ -15,19 +16,27 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+  candidateName = input.question("What is your name? ");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-
+  candidateAnswer = input.question(question);
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
+  console.log("\nCandidate Name: " + candidateName);
+  console.log("1) " + question);
+  console.log("Your Answer: " + candidateAnswer);
+  console.log("Correct Answer: " + correctAnswer);
+  if (candidateAnswer === correctAnswer){
+    console.log("\nYou entered the Correct Answer!");
+  }
+  else {
+    console.log("\nYou entered an Incorrect Answer!");
+  }
 
   let grade;
   
@@ -39,6 +48,9 @@ function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
   
+  // write a message to the console greeting the user using the name they just provided.
+  console.log("Greetings, " + candidateName + "!\n");
+
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
